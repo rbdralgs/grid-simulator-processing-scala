7f95d258028c076fb0e7b1d0b5a2e3ccb2765653
Rbdr Algs <rbdralgs@gmail.com>
2016-08-11 20:19:01 +0200

    Init Setup

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "grid-simulator-processing-scala"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+
+libraryDependencies += "org.processing" % "core" % "3.1.1"
+
+resolvers += "Processing Core" at "https://mvnrepository.com/artifact/org.processing/core"
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,25 @@
+import processing.core.PApplet
+
+class Main extends PApplet {
+
+  override def setup: Unit = {
+  }
+  
+  
+  override def settings: Unit = {
+    size(800, 800)
+  }
+
+
+  override def draw: Unit = {
+  }
+}
+
+object Main {
+  def main(args:Array[String]) {
+    PApplet.main(Array("Main"))
+  }
+
+
+
+}
